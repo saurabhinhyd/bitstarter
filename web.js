7c4b96a12800db85ff5f0a15165a4e6a94a41df4
Saurabh Agrawal <saurabhinhyd@gmail.com>
2013-07-09 14:27:29 +0000

    Updated web.js to read from file

--- web.js
+++ web.js
@@ -1,10 +1,17 @@
+var fs = require('fs');
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+fs.readFile('index.html', 'utf-8', function (err, data) {
+  if (err) throw err;
+  app.get('/', function(request, response) {
+    response.send(data);
+  });
 });
+
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
