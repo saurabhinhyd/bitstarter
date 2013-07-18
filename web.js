09a7086775684ac5884a125fa46a2da44c964010
Saurabh Agrawal <saurabhinhyd@gmail.com>
2013-07-18 13:47:26 +0000

    update web.js to return the most recent index.html on a GET request

--- web.js
+++ web.js
@@ -1,19 +1,15 @@
 var fs = require('fs');
 var express = require('express');
+var htmlfile = "index.html";
 
 var app = express.createServer(express.logger());
 
-fs.readFile('index.html', 'utf-8', function (err, data) {
-  if (err) throw err;
-  app.get('/', function(request, response) {
-    response.send(data);
-  });
+app.get('/', function(request, response) {
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-
-
-
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
